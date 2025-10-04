610b463b63e349e9e567a31990a46de17cbc38a0
sandyddeveloper <sandydeveloper@gmail.com>
2025-10-04 15:18:00 +0530

    init

--- partternProgram/PatternProgram8.java
+++ partternProgram/PatternProgram8.java
@@ -1,7 +1,28 @@
+/* WAJP THE FOLLOWING PATTERN BY USING NEXTED FOR LOOP STATAMENT
+
+O/P: ***
+	 ***
+	 ***
+
+*/
+
 class PatternProgram8 
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		int n = 3; // this is the size for the matrix 3*3 rows&cols
+		for (int i = 0; i < n ; i ++ ) // outer Loop for cols
+		{
+			for (int j = 0; j < n ; j ++ ) // inner Loop for rows
+			{
+				if (true)
+				{
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println(); // this is for seperate the * like row wise.. without this the o/p will be like **********
+		}
 	}
 }
